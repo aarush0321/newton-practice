d1899093c98e25e9d057159831386c58f0c2dc40
aarush0321 <aarush03@berkeley.edu>
2025-08-20 12:16:13 -0700

    Fixed Issue, separated derivative function to two functions

--- newton.py
+++ newton.py
@@ -1,13 +1,15 @@
 def derivative(epsilon, x, function):
-    first_derivative = (function(x+epsilon)-function(x-epsilon))/(2*epsilon)
-    second_derivative = (function(x+epsilon) - 2*function(x) + function(x-epsilon))/(epsilon**2)
-    return first_derivative, second_derivative
+    return (function(x+epsilon)-function(x-epsilon))/(2*epsilon)
 
+def second_derivative(epsilon, x, function):
+    return (function(x+epsilon) - 2*function(x) + function(x-epsilon))/(epsilon**2)
+    
 def newton_optimization(x0, function, epsilon = 0.005):
     tolerance = 0.5
     update = 100
     while update >= tolerance:
-        first_derivative, second_derivative = derivative(epsilon, x0, function)
+        first_derivative = derivative(epsilon, x0, function)
+        second_derivative = second_derivative(epsilon, x0, function)
         next_x = x0 - first_derivative/second_derivative
         update = next_x - x0
         x0 = next_x
